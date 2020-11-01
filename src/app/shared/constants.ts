e286646a2cec6c9512fa67174a315c6a0d6f7741
Anuran Datta <anurdatt@in.ibm.com>
2020-11-02 02:36:49 +0530

    heroku and mobile changes

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,4 +1,8 @@
-export const CAMERA_API_URL = 'http://localhost:3000/api/cam';
-export const EMPLOYEE_API_URL = 'http://localhost:3000/api/emp';
-export const WEB_SOCKET_URL = 'ws://localhost:3000';
-export const RTSP_WS_URL = 'ws://localhost:9999';
+//export const CAMERA_API_URL = 'http://localhost:3000/api/cam';
+export const CAMERA_API_URL = 'https://face-identification-be.herokuapp.com/api/cam';
+//export const EMPLOYEE_API_URL = 'http://localhost:3000/api/emp';
+export const EMPLOYEE_API_URL = 'https://face-identification-be.herokuapp.com/api/emp';
+//export const WEB_SOCKET_URL = 'ws://localhost:3000';
+export const WEB_SOCKET_URL = 'wss://face-identification-be.herokuapp.com';
+// export const RTSP_WS_URL = 'ws://localhost:9999';
+export const RTSP_WS_URL = 'wss://face-identification-be.herokuapp.com:9999';
